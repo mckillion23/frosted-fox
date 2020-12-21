756421fad8ee5b5fa98ae1ea64fdcd3af3a7fe28
McKillion, Mark <mark.mckillion@rakuten.com>
2020-12-21 13:58:10 +0000

    Go to service and database

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React from 'react';
 import Facebook from "../../components/features/Facebook";
+import axios from 'axios';
 
-const About = () => (
-    <div className="mt-10 flex flex-col items-center pb-24">
-        About us information coming soon
-        <br/>
-        <br/>
-        <span className="text-brand">To contact us please use our facebook page!</span>
-        <br/>
-        <Facebook />
-    </div>
-)
-export default About
+export default class About extends React.Component {
+  state = {
+    blogItems: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://frosted-fox-service.herokuapp.com/`)
+      .then(res => {
+        const blogItems = res.data;
+        this.setState({ blogItems });
+      })
+  }
+
+  render() {
+    return (
+        <div className="mt-10 flex flex-col items-center pb-24">
+            About us information coming soon
+            <br/>
+            <br/>
+            <span className="text-brand">To contact us please use our facebook page!</span>
+            <br/>
+            <Facebook />
+            Blog Items:
+            <ul>
+                { this.state.blogItems.map(blogItem => <li>{blogItem.text}</li>)}
+            </ul>
+        </div>
+    )
+  }
+}
