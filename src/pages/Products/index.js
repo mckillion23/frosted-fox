e74f3bf7f706566a41c24a82e8d27316a9b03164
McKillion, Mark <mark.mckillion@rakuten.com>
2020-12-22 16:23:06 +0000

    Add products

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import Facebook from "../../components/features/Facebook";
+import Prismic from "prismic-javascript";
+
+const apiEndpoint = "https://frosted-fox.cdn.prismic.io/api/v2";
+
 export default class Products extends React.Component {
+  state = {
+    results: []
+  }
+
+  async componentDidMount() {   
+    const api = await Prismic.getApi(apiEndpoint);
+    const response = await api.query("");
+    this.setState({ results: response.results })
+  }
+
   render() {
     return (
         <div className="mt-10 flex flex-col items-center pb-24">
-        Product information coming soon
-        <br/>
-        <br/>
-        <span className="text-brand">To contact us please use our facebook page!</span>
-        <br/>
-        <Facebook />
+        Products
+          { this.state.results.map(result =>
+            <p>
+              Title: {result.data.product_title[0].text}
+              <br/>
+              Info: {result.data.product_text[0].text}
+              <br/>
+              Photo: <img src={result.data.product_image.url} alt={result.id} />
+            </p>
+          )}
     </div>
     )
   }
